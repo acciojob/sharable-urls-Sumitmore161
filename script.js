b604d0e36b2ebedae82e4d1b146bbccdbc8fde6c
Sumitmore161 <127652844+Sumitmore161@users.noreply.github.com>
2026-05-17 18:09:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+document.getElementById("myForm").addEventListener("submit", function (e) {
+	e.preventDefault();
+
+	const name = document.getElementById("name").value;
+	const year = document.getElementById("year").value;
+
+	const params = new URLSearchParams(window.location.search);
+	params.set("name",name);
+	params.set("year",year);
+	window.location.search = params.toString();
+})
