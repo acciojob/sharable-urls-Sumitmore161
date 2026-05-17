af8a363b90bd120e623f01254e162a2bef7948ba
Sumitmore161 <127652844+Sumitmore161@users.noreply.github.com>
2026-05-17 18:30:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // your code here
+	const myUrl = document.getElementById("myUrl");
+	myUrl.textContent = window.location.origin + window.location.pathname
 document.getElementById("myForm").addEventListener("submit", function (e) {
 	e.preventDefault();
 
 	const name = document.getElementById("name").value;
 	const year = document.getElementById("year").value;
-	const myUrl = document.getElementById("myUrl");
 	const params = new URLSearchParams(window.location.search);
-	params.set("name",name);
-	params.set("year",year);
-	myUrl.textContent = window.location.search;
-	window.location.search = params.toString();
+	if(name)
+		params.set("name",name);
+	if(year)
+		params.set("year",year);
+	const finalUrl = window.location.origin + window.location.pathname + "?" + params.toString();
+	myUrl.textContent = finalUrl;
+	window.location.search = finalUrl;
 	
 })
